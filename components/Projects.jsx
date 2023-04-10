323043c328298bf2208ae3e74b8b15ba680412bb
Alexander Morais <alexmoraiswilsoft@gmail.com>
2023-04-10 16:31:46 -0400

    Agregado la sección de proyectos

---
+++ components/Projects.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import gitHubImage from "./../public/assets/projects/github.png";
+
+const Projects = () => {
+    return (
+        <div className="max-w-[1240px] mx-auto px-2 py-16">
+            <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+                Proyectos
+            </p>
+            <h2 className="py-4">Proyectos que he desarrollado</h2>
+            <div className="grid md:grid-cols-2 gap-8">
+                {/*GitHub*/}
+                <ProjectItem
+                    title="GitHub"
+                    technology="Tecnología"
+                    backgroundImage={gitHubImage}
+                    projectUrl="/"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import About from "../components/About";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 export default function Home() {
     return (
@@ -29,6 +30,8 @@ export default function Home() {
             {/* Skills */}
             <Skills />
 
+            {/* Projects */}
+            <Projects />
         </>
     );
 }
